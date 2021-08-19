27d1e3b4d4a0e4324485204e8a83c086dcf889b2
Amber Ramesh <amramesh@iu.edu>
2021-08-19 16:55:54 -0400

    Add JSDoc for asctb functions

--- build/asctb-shared.js
+++ build/asctb-shared.js
@@ -1,25 +1,25 @@
 
 /**
-  Get a list of key-value pairs nested within a Map.
-  @param {Array} entry A [key, value] entry.
-  @returns {Array<Array>|undefined} An array of key-value pairs, if the nested value is a Map object.
-
- **/
+ * Get a list of key-value pairs nested within a Map.
+ * @param {Array} entry A [key, value] entry.
+ * @returns {Array<Array> | undefined} An array of key-value pairs, if the nested value is a Map object.
+ */
 const getChildren = ([, value]) => value instanceof Map && Array.from(value);
 /**
-  Get the title of a D3.hierarchy node.
-  @param {Object} node: A D3.hierarchy node or object with data property.
-  @param {Array} node.data: The data property of the node.
-  @returns {String} The title of the node.
-**/
+ * Get the title of a D3.hierarchy node.
+ * @param {object} node: A D3.hierarchy node or object with data property.
+ * @param {Array} node.data: The data property of the node.
+ * @returns {string} The title of the node.
+ */
 const getTitle = ({ data }) => data[0] ?? '';
 
 /**
-  Get the ASCT+B table rows for a D3.hierarchy node.
-  @param {Object} node A D3.hierarchy node or object with data property.
-  @param {Array} node.data The data property of the node.
-  @returns {Generator} A generator that iterates through data objects corresponding to a node.
-**/
+ * Get the ASCT+B table rows for a D3.hierarchy node.
+ * @generator
+ * @param {object} node A D3.hierarchy node or object with data property.
+ * @param {Array} node.data The data property of the node.
+ * @yields {object} A CSV data row corresponding to the node.
+*/
 function* dataAccessor({ data }) {
   if (!Array.isArray(data)) return {};
   const [, value] = data;
@@ -36,20 +36,18 @@ function* dataAccessor({ data }) {
 }
 
 /**
-  Indicates if a D3.hierarchy object represents an FTU or its component.
-  @param {Object} node A D3.hierarchy node or object with data property.
-  @param {Array} node.data The data property of the node.
-  @returns {Boolean} A boolean indicating if the node is an FTU.
-**/
+ * Indicates if a D3.hierarchy object represents an FTU or its component.
+ * @param {object} node A D3.hierarchy node or object with data property
+ * @returns {boolean} A boolean indicating if the node is an FTU. 
+ */
 function isFTUNode(node) {
   return [...dataAccessor(node)].every(data => !!data['FTU']);
 }
 
 /**
-  Indicates if a D3.hierarchy object represents a cell type.
-  @param {Object} node A D3.hierarchy node or object with data property.
-  @param {Array} node.data The data property of the node.
-  @returns {Boolean} A boolean indicating if the node represents a cell type.
+ * Indicates if a D3.hierarchy object represents a cell type.
+ * @param {object} node A D3.hierarchy node or object with data property.
+ * @returns {boolean} A boolean indicating if the node represents a cell type.
 **/
 function isCellTypeNode(node) {
   const [key, value] = node.data;
